c5523fd2c298c2651a5f88f99c94335cf41e9402
Matthew <matt22torres@gmail.com>
2014-04-19 15:11:40 -0500

    Changed scenes that are loaded
    Added a start screen that has a button for a main menu screen.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI ()
+    {
+        //Create a button that returns true or false
+        if (GUI.Button(new Rect (Screen.width/2, Screen.height/2, 100, 100), "Start"))
+        {
+            //Open a new scene
+            Application.LoadLevel("MenuScreen");
+        }
+    }
+}
