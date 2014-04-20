eee8aa3e0e967345463e4027fbe959ee0eb0741c
orbv <orbv16@gmail.com>
2014-04-20 02:57:16 -0500

    Added dick missile

---
+++ Assets/_Scripts/ProjectileLauncher.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileLauncher : MonoBehaviour {
+
+	// Class Members
+	public GameObject missile;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+		{
+			// Local Variables
+			GameObject thisMissile;
+			Vector3 position = new Vector3(0, 0, 0) * 10.0f;
+			
+			position = transform.TransformPoint(position);
+			
+			thisMissile = (GameObject)GameObject.Instantiate(missile, position, transform.localRotation);
+			Physics.IgnoreCollision(thisMissile.collider, collider);
+		}
+	}
+}
