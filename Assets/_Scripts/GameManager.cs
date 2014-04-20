22c67514c2d8ade006006eaae412affc76f23398
Matthew <matt22torres@gmail.com>
2014-04-20 11:04:10 -0500

    Added sweet title to Start screen

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -2,10 +2,13 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+    //Remove this when we have stuff
+    private Font _logoFont;
+    private GUIStyle _logoStyle;
 
 	// Use this for initialization
 	void Start () {
-	
+        
 	}
 	
 	// Update is called once per frame
@@ -14,12 +17,22 @@ public class GameManager : MonoBehaviour {
 	}
 
     void OnGUI ()
-    {
-        //Create a button that returns true or false
-        if (GUI.Button(new Rect (Screen.width/2, Screen.height/2, 100, 100), "Start"))
-        {
-            //Open a new scene
-            Application.LoadLevel("MenuScreen");
-        }
+    {        
+        //GUI.skin.font = _logoFont;
+        _logoStyle.fontSize = Screen.width / 20;
+        _logoStyle.alignment = TextAnchor.MiddleCenter;        
+
+        GUILayout.BeginArea(new Rect(Screen.width / 2 - Screen.width / 4, Screen.height / 2 - 100, Screen.width / 2, 200));
+            GUILayout.Label("TopDownSpace",_logoStyle);
+
+            GUILayout.Space(15);
+
+            //Create a button that returns true or false
+            if (GUILayout.Button("Start"))
+            {
+                //Open a new scene
+                Application.LoadLevel("MenuScreen");
+            }
+        GUILayout.EndArea();
     }
 }
