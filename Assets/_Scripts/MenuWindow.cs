e11513128214f647569c0f989f9a7b621a1cbee3
Matthew <matt22torres@gmail.com>
2014-04-19 19:35:45 -0500

    Added a menu when you right click on menu screen.

---
+++ Assets/_Scripts/MenuWindow.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuWindow : MonoBehaviour {
+    private bool openMenu = false;
+    public Rect menuWindow;
+    private int windowWidth = 200;
+    private int windowHeight = 200;
+
+    private Vector3 screenPosition;
+	// Use this for initialization
+	void Start () {
+        menuWindow = new Rect(Screen.width / 2 - windowWidth / 2, Screen.height / 2 - windowHeight / 2, windowWidth, windowHeight);
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        openMenu = Input.GetMouseButton(1);
+    }
+
+    void OnGUI ()
+    {
+        if(openMenu){
+            menuWindow = GUILayout.Window(0,menuWindow,DoOpenMenu,"");
+        }
+    }
+
+    void DoOpenMenu(int windowID)
+    {
+        if (GUILayout.Button("Play Game"))
+        {
+            Application.LoadLevel("GameScreen");
+        }
+        if (GUILayout.Button("Options"))
+        {
+
+        }
+    }
+}
