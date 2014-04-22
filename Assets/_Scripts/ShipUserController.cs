a0bbe8b5bab68faf418815f057d3ea1b62c39b42
Matthew <matt22torres@gmail.com>
2014-04-21 22:48:03 -0500

    Multiplayer controls

--- Assets/_Scripts/ShipUserController.cs
+++ Assets/_Scripts/ShipUserController.cs
@@ -3,10 +3,11 @@
 [RequireComponent(typeof(ShipController))]
 public class ShipUserController : Photon.MonoBehaviour {
 
-    //public float rotationSpeed = 160; // 160 = best rotation speed
-    //private ShipController ship;
-	
-    //void Awake () {
+    public float rotationSpeed = 160; // 160 = best rotation speed
+    private ShipController ship;
+
+    //void Awake()
+    //{
     //    ship = GetComponent<ShipController>();
     //}
 	
@@ -37,7 +38,39 @@ public class ShipUserController : Photon.MonoBehaviour {
             transform.position = Vector3.Lerp(transform.position, this.correctPlayerPos, Time.deltaTime * 5);
             transform.rotation = Quaternion.Lerp(transform.rotation, this.correctPlayerRot, Time.deltaTime * 5);
         }
+        else if (photonView.isMine)
+        {
+            ship = GetComponent<ShipController>();
+            //rotate ship around y axis
+            transform.Rotate(0.0f, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime, 0.0f);
+            // clamps boundries for map which are currently hardcoded
+            // the clamp is needed to prevent the rotation physics to move the ship +- on the y axis which is baaaaad
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -500, 500),0.0f,Mathf.Clamp(transform.position.z,-500,500));
+        }
     }
+
+    void FixedUpdate()
+    {
+        if (photonView.isMine)
+        {
+            ship = GetComponent<ShipController>();
+            // get input for rotation and the forward/reverse movement
+            float rotate = CrossPlatformInput.GetAxis("Horizontal");
+            float throttle = CrossPlatformInput.GetAxis("Vertical");
+
+            // pass the input to the ship
+            ship.Move(rotate, throttle);
+        }
+    }
+
+    //void Update()
+    //{
+    //    // rotate ship around y axis
+    //    transform.Rotate(0.0f, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime, 0.0f);
+    //    // clamps boundries for map which are currently hardcoded
+    //    // the clamp is needed to prevent the rotation physics to move the ship +- on the y axis which is baaaaad
+    //    transform.position = new Vector3(Mathf.Clamp(transform.position.x, -500, 500), 0.0f, Mathf.Clamp(transform.position.z, -500, 500));
+    //}
 
     void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
